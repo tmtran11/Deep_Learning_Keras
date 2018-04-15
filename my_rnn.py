a94c85ae33b1510d3d440dd4aa5a0322b2efd529
Trang Tran <tmtran@knox.edu>
2018-04-15 01:09:44 -0500

    Update my_rnn.py

--- my_rnn.py
+++ my_rnn.py
@@ -22,7 +22,8 @@ sc = MinMaxScaler()
 training_set_scaled = sc.fit_transform(training_set)
 indicator_set_scaled = sc.transform(indicator_set)
 
-# Seperate training set
+# Using data of 120 days before to predict the stock price of one day
+# Training set, Using Google stock price itself
 X_train = []
 y_train = []
 for i in range(120, 1762):
@@ -30,23 +31,26 @@ for i in range(120, 1762):
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
+# Indicator set, Using Microsoftword stock price
+# Expect this extra parameter to better tune the predictive model
 X_indicator = []
 y_indicator = []
 for i in range(120, 1762):
-    X_indicator.append(training_set_scaled[(i-120):i, 0])
-    y_indicator.append(training_set_scaled[i, 0])
+    X_indicator.append(indicator_set_scaled[(i-120):i, 0])
+    y_indicator.append(indicator_set_scaled[i, 0])
 X_indicator, y_indicator = np.array(X_indicator), np.array(y_indicator)
 
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 X_indicator = np.reshape(X_indicator, (X_indicator.shape[0], X_indicator.shape[1], 1))
 
+# 120 column from training set + 120 column from indicator set
 X_train = np.concatenate((X_train, X_indicator), axis = 2)
 
 # Build recurrent neural neural network
-from keras.models import Sequential # mode sequential   
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+from keras.models import Sequential # sequential layers
+from keras.layers import Dense      # Basics Neural Network layers
+from keras.layers import LSTM       # Long Short Term Memory cells 
+from keras.layers import Dropout    # prevent over fitting
 
 
 regressor = Sequential()
@@ -67,7 +71,9 @@ regressor.add(Dropout(0.2))
 regressor.add(Dense(units = 1))
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+# pass in by batches of 32 samples and iterate for 100 time
 
+# prepare test set
 dataset_test1 = pd.read_csv('GOOG_test.csv')
 dataset_test2 = pd.read_csv('MSFT_test.csv')
 real_stock_price_1 = dataset_test1.iloc[:, 1:2].values
@@ -90,7 +96,7 @@ X_test1 = np.reshape(X_test1, (X_test1.shape[0], X_test1.shape[1], 1))
 
 
 X_test2 = []
-for i in range(120, 367): #fix
+for i in range(120, 367): 
     X_test2.append(inputs2[(i-120):i, 0])   
 X_test2 = np.array(X_test2)
 X_test2 = np.reshape(X_test2, (X_test2.shape[0], X_test2.shape[1], 1))
@@ -100,6 +106,7 @@ X_test = np.concatenate((X_test1, X_test2), axis = 2)
 pred_stock_price = regressor.predict(X_test)
 pred_stock_price = sc.inverse_transform(pred_stock_price)
 
+# plot to see the result
 plt.plot(real_stock_price_1, color = 'red', label = 'real')
 plt.plot(pred_stock_price, color = 'blue', label = 'pred')
 plt.legend()
